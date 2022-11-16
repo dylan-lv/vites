dc6c12e720ef821d1bfc5fc38a36f3d16a520344
lvhaizhou <417703682@qq.com>
2022-11-16 15:18:57 +0800

    feat(vites): vite 3.x 版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
-import path from "path";
-import { defineConfig } from "vite";
-import Vue from "@vitejs/plugin-vue";
-import VueJsx from "@vitejs/plugin-vue-jsx";
+import path from 'path';
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import VueJsx from '@vitejs/plugin-vue-jsx';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "~": `${path.resolve(__dirname, "src")}/`,
+      '~': `${path.resolve(__dirname, 'src')}/`,
     },
   },
   plugins: [
-    Vue({ reactivityTransform: true }),
+    Vue(),
     VueJsx(),
   ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        /* 引入var.scss全局预定义变量，多个：'@import "xxx";@import "xxx' */
-        additionalData: "@import '~/style/global.scss';",
-      },
-    },
-  },
 });
