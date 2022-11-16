ac9cef0d8b94e8ebcf22cedb290e87f3ad67bf1a
lvhaizhou <417703682@qq.com>
2022-11-16 11:19:15 +0800

    chore(auto-import): 删掉自动引入

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import VueJsx from "@vitejs/plugin-vue-jsx";
-import AutoImport from "unplugin-auto-import/vite";
 
 export default defineConfig({
   resolve: {
@@ -13,16 +12,6 @@ export default defineConfig({
   plugins: [
     Vue({ reactivityTransform: true }),
     VueJsx(),
-    AutoImport({
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/, // .vue
-        /\.md$/, // .md
-      ],
-      dts: "./auto-imports.d.ts",
-      imports: ["vue", "vue-router"],
-    }),
   ],
   css: {
     preprocessorOptions: {
